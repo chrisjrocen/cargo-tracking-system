7db48bb2d4c5b874a351862b7b7e5e0cc6a1a7f7
chrisjrocen <chrisocen4@gmail.com>
2023-08-03 00:18:06 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>carTTrack</h1>
+        <p>Cargo Tracking Software</p>
+        <code>Coming soon...</code>
+
+      </>
+      )
+}
+
+      export default App
