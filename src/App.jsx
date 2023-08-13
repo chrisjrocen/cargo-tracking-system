9f5fe2d618b69c6f1077f82d78785ea5cdbc1718
chrisjrocen <chrisocen4@gmail.com>
2023-08-13 23:59:30 +0300

    added main page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
+import React from 'react';
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>carTTrack</h1>
+      <div>
+        <Header />
+      </div>
+      <div className='h-screen'>
+        <h1>carTTrack</h1>
         <p>Cargo Tracking Software</p>
         <code>Coming soon...</code>
+      </div>
+      <div>
+        <Footer />
+      </div>
 
-      </>
-      )
+    </>
+  )
 }
 
-      export default App
+export default App
---
+++ src/components/Footer.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+
+
+function Footer() {
+
+    return (
+        <footer className="bg-gray-200 py-6 text-black text-left">
+        <div className="max-w-6xl mx-auto px-4">
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+            {/* Column 1 */}
+            <div>
+              <h3 className="text-xl font-semibold mb-3">Address</h3>
+              <ul className="list-disc list-inside list-none">
+                <li>Lake house, Kampala road</li>
+              </ul>
+            </div>
+  
+            {/* Column 2 */}
+            <div>
+              <h3 className="text-xl font-semibold mb-3">Contact</h3>
+              <ul className="list-disc list-inside list-none">
+                <li>071-2345678</li>
+                <li>info@cattrack.com</li>
+              </ul>
+            </div>
+  
+            {/* Column 3 */}
+            <div>
+              <h3 className="text-xl font-semibold mb-3">Opening hours</h3>
+              <ul className="list-disc list-inside list-none">
+                <li>Monday to Friday</li>
+                <li>8:00am - 5:00pm</li>
+              </ul>
+            </div>
+  
+            {/* Column 4 */}
+            <div>
+              <h3 className="text-xl font-semibold mb-3">Social media</h3>
+              <ul className="list-disc list-inside list-none">
+                <li>x: @titans</li>
+                <li>Instagram: @titans</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </footer>
+    )
+}
+
+export default Footer
